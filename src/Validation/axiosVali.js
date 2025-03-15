d5eb9e97bb7a118246d27c9c482f9b1ce9aca652
CodingPoka <22103351@iubat.edu>
2025-03-16 01:39:27 +0600

    hi

--- src/Validation/axiosVali.js
+++ src/Validation/axiosVali.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://mernauth4-backend.vercel.app/', // Backend API URL
+  baseURL: 'https://mernauth4-backend.vercel.app/api', // Backend API URL
   withCredentials: true, // To send cookies (JWT token) with each request
 });
 
